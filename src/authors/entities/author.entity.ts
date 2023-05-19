3431a0271cda8b121979e6075870b4ea05793423
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-05-19 16:59:44 -0600

    avance de Author

--- src/authors/entities/author.entity.ts
+++ src/authors/entities/author.entity.ts
@@ -1,7 +1,19 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Post } from 'src/posts/post.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Author {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany(() => Post, (post) => post.autor)
+  @Field(() => [Post], { nullable: true })
+  posts: Post[];
 }
